1e90049ceb071870d61ac221e817402eda5243d4
Vilon <fangliang@live.cn>
2018-07-27 16:34:17 +0800

    ui框架

---
+++ Assets/LuaFramework/Lua/View/PromptPage.lua
@@ -0,0 +1,32 @@
+local transform;
+local gameObject;
+
+PromptPage = {};
+local this = PromptPage;
+
+--启动事件--
+function PromptPage.Awake(obj)
+	gameObject = obj;
+	transform = obj.transform;
+
+	this.InitPanel();
+	logWarn("Awake lua--->>"..gameObject.name);
+end
+
+--初始化面板--
+function PromptPage.InitPanel()
+	logError("PromptPage.InitPanel")
+	this.btnOpen = transform:Find("Open").gameObject;
+	print(this.btnOpen.name)
+	this.gridParent = transform:Find('ScrollView/Grid');
+	print(this.gridParent.name)
+end
+
+function PromptPage.Start()
+	-- body
+end
+
+--单击事件--
+function PromptPage.OnDestroy()
+	logWarn("OnDestroy---->>>");
+end
