7e8667842e9cc961c0861f7ab516af626786305b
Vilon <fangliang@live.cn>
2018-07-30 16:18:56 +0800

    ios android资源分离

--- Assets/LuaFramework/Scripts/ConstDefine/AppConst.cs
+++ Assets/LuaFramework/Scripts/ConstDefine/AppConst.cs
@@ -20,7 +20,7 @@ namespace LuaFramework
                 return updateMode;
             }
         }
-        public static bool LuaByteMode = true;                       //Lua字节码模式-默认关闭 
+        public static bool LuaByteMode = false;                       //Lua字节码模式-默认关闭 
         public static bool LuaBundleMode = false;                    //Lua代码AssetBundle模式
 
         public const int TimerInterval = 1;
@@ -32,8 +32,11 @@ namespace LuaFramework
         public const string AppPrefix = AppName + "_";              //应用程序前缀
         public const string ExtName = ".unity3d";                   //素材扩展名
         public const string AssetDir = "StreamingAssets";           //素材目录 
-        public const string WebUrl = "http://192.168.0.4/";      //测试更新地址
-
+#if UNITY_ANDROID
+        public const string WebUrl = "http://192.168.0.4/Android/"; //测试更新地址
+#elif UNITY_IPHONE
+        public const string WebUrl = "http://192.168.0.4/iOS/";      //测试更新地址
+#endif
         public static string UserId = string.Empty;                 //用户ID
         public static int SocketPort = 0;                           //Socket服务器端口
         public static string SocketAddress = string.Empty;          //Socket服务器地址
